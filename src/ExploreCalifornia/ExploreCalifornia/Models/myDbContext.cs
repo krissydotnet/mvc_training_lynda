93f6a939dbe653aa8b3e94457b5fa55cbe9fcbe1
Krissy Dukes <krissydotnet@yahoo.com>
2019-01-31 12:56:45 -0500

    Training in progress

---
+++ src/ExploreCalifornia/ExploreCalifornia/Models/myDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ExploreCalifornia.Models
+{
+    public class myDbContext : DbContext
+    {
+        public myDbContext() : base("ExploreCalifornia")
+        {
+
+        }
+        public DbSet<Tour> Tours { get; set; }
+    }
+}
